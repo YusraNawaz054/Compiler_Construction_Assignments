028c687a9e972a2420af3fd793b5f4597a706005
YusraNawaz054 <113548080+YusraNawaz054@users.noreply.github.com>
2023-09-25 22:36:26 +0500

    Add files via upload

---
+++ 2nd-Lab/Task-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LogicalOperatorsRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Regular expression pattern for detecting logical operators
+            string pattern = @"&&|\|\||!";
+
+            Console.WriteLine("Enter a text to find logical operators:");
+            string input = Console.ReadLine();
+
+            // Use Regex to find and display logical operators in the input
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            if (matches.Count > 0)
+            {
+                Console.WriteLine("Logical operators found:");
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No logical operators found in the input.");
+            }
+        }
+    }
+}
