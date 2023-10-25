e62f3bf073bb2c2cafb12318f1575f891ab72da9
YusraNawaz054 <113548080+YusraNawaz054@users.noreply.github.com>
2023-10-25 15:00:32 +0500

    Add files via upload

---
+++ Mid_lab/q2/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace RecursiveDescentLabMid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.parse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.AutoSize = true;
+            this.output.Location = new System.Drawing.Point(366, 48);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(0, 13);
+            this.output.TabIndex = 0;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(198, 130);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(360, 98);
+            this.inputBox.TabIndex = 1;
+            // 
+            // parse
+            // 
+            this.parse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.parse.Location = new System.Drawing.Point(340, 261);
+            this.parse.Name = "parse";
+            this.parse.Size = new System.Drawing.Size(75, 42);
+            this.parse.TabIndex = 2;
+            this.parse.Text = "Press for Parsing";
+            this.parse.UseVisualStyleBackColor = true;
+            this.parse.Click += new System.EventHandler(this.parse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 362);
+            this.Controls.Add(this.parse);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.output);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label output;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button parse;
+    }
+}
+
